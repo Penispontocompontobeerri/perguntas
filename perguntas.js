06b9d5044dfa53eaf88f656cfd66d8c59044c24f
ian_lindo <00001080399276sp@al.educacao.sp.gov.br>
2024-10-24 11:57:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'GARGALHAR',
+    'Sexo é como ir a pesca',
+    'O importante não é o tamanho da cana, e sim o trabalho da minhoca'
+)
+
+criaCartao(
+    'HISTORIA',
+    'O que as pernas de um paraplégico tem em comum com o comunismo?',
+    'Ambos não funcionam'
+)
+
+criaCartao(
+    'LEGUMES',
+    'Sabe qual a parte mais dura de um vegetal?',
+    'A cadeira de rodas'
+)
+
+
+
+
+
+
+criaCartao(
+    'VOCÊ',
+    'Se você está leno essa mensagem?',
+    'É um sinal que evoluímos tanto que veados agora sabem ler'
+)
